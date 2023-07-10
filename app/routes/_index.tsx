a53243c4c36e6d0158aca2b4d6a578279c238d20
liranmeir <liranmeir@gmail.com>
2023-07-10 14:02:23 +0300

    rename to MNB VIDEOS

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,12 +12,12 @@ export default function IndexRoute() {
     <div className="container">
       <div className="content">
         <h1>
-          Remix <span>Jokes!</span>
+          MNB <span>Videos</span>
         </h1>
         <nav>
           <ul>
             <li>
-              <Link to="jokes">Read Jokes</Link>
+              <Link to="jokes">View</Link>
             </li>
           </ul>
         </nav>
